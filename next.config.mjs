f588db0a7dc74df0ec59e4d24803d287a58867d0
Jackson Ling <jackson.llk@gmail.com>
2024-07-19 11:57:56 +0800

    remove export

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'out',  // Ensure this matches the output directory
-  output: 'export' // Use the new export configuration
 };
 
 export default nextConfig;
