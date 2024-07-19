d1fc0529d4b5255c11645c0bcb82e986495a0ed6
Jackson Ling <jackson.llk@gmail.com>
2024-07-19 11:28:10 +0800

    added webpack bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'out',
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        reportFilename: 'bundles/client.html',
+        openAnalyzer: false,
+      }));
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
