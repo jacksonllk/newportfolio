63df99c79810f24c9f78c18486b4295f0e095e24
Jackson Ling <jackson.llk@gmail.com>
2024-07-29 09:44:35 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,8 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+/** @type {import('next').NextConfig} */
 
 const nextConfig = {
   reactStrictMode: true,
   distDir: 'out',  // Ensure this matches the output directory
-  webpack: (config) => {
-    // Example: Splitting vendor code into separate chunks
-    config.optimization.splitChunks = {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    };
-
-    // Example: Limiting the maximum size of chunks to reduce size of 0.pack
-    config.performance = {
-      maxEntrypointSize: 20000,
-      maxAssetSize: 20000,
-    };
-
-    return config;
-  },
 };
 
 export default nextConfig;
