10b002fe60ccc8d7e8a4e3d3a0b708ab3b7d0f17
Jackson Ling <96425149+jacksonllk@users.noreply.github.com>
2024-08-12 20:43:09 +0800

    testing 6

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,11 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  // output: 'export',
+  output: 'export',
   distDir: 'out',  // Ensure this matches the output directory
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
